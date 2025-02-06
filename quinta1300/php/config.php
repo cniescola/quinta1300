182d2a18307463eae29bbe766e5b2f76b41c3ac5
CiborgLuiz <126992162+CiborgLuiz@users.noreply.github.com>
2025-02-06 14:31:26 -0300

    Add files via upload

---
+++ quinta1300/php/config.php
@@ -0,0 +1,16 @@
+<?php
+    $nome = $_POST['nome'];
+    $sobrenome = $_POST['sobrenome'];
+    $endereco = $_POST['endereco'];
+    $email = $_POST['email'];
+
+    echo "Seu nome: $nome <br>";
+    echo "Seu sobrenome: $sobrenome <br>";
+    echo "Sua endereco: $endereco <br>";
+    echo "Sua email: $email <br>";
+
+
+
+
+
+?>
