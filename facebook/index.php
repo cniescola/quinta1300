60fe32a4210c98c95c9f21033c2f8b79c0d58d2a
vitor brenno <cniescola85@gmail.com>
2025-02-27 14:06:49 -0300

    adicionando imagens

---
+++ facebook/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <main>
+        <div id="esquerda">
+            <div id="logo">
+                <img src="face.png" alt="">
+            </div>
+            <div id="negona">
+                <img src="negona.png" alt="">
+            </div>
+            <div id="texto"></div>
+        </div>
+        <div id="direita"></div>
+    </main>
+
+</body>
+</html>
