9d7ec8ccafb68261abe6b33111955035c708e92a
Raphael Cury <raphaelcury00@gmail.com>
2025-03-29 01:02:43 -0300

    commit da segunda pagina que faltava pra comercarmos com php

---
+++ dev_plugin/php/importexcel.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Import Excel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style2.css">
+
+  </head>
+  <body>
+
+    <div id="geral">
+        
+        <div id="cabecalho">
+           <div id="textcabecalho">SISTEMA DE UPLOAD DADOS VIA EXCEL</div>
+        </div>
+
+        <div id="bottonplanilha">
+            <div id="verdin">
+                <div id="textverdin">CLIQUE ABAIXO PARA BUSCAR PLANILHA</div>
+            </div>
+        </div>
+
+        <div id="retangulozada">
+
+            <div id="add">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                  </svg>
+            </div>
+
+        </div>
+    
+        <div id="rodapezito">
+
+            <div id="roxin">
+                <div id="textroxin">FAZER UPLOAD DE PLANILHA</div>
+            </div>
+
+        </div>
+    
+    
+    </div>
